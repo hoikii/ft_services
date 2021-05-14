ea636cc649da87ccfc8bf1a8f14ade0915285f3e
kanlee <kanlee@student.42seoul.kr>
2021-05-14 20:05:32 +0900

    enable metallb and apply configmap

--- setup.sh
+++ setup.sh
@@ -4,12 +4,18 @@ CCBLUE="\033[34m"
 CCBLUE_BOLD="\033[1;34m"
 CCEND="\033[0m"
 
-echo "$CCBLUE >>> starting minikube <<< $CCEND"
+echo "$CCBLUE_BOLD >>> starting minikube <<< $CCEND"
 minikube start --driver=virtualbox
 IP=`minikube ip`
 echo "minikube IP = $IP"
 sed -i '' "s/___MINIKUBE_IP___/$IP/" srcs/metallb-configmap.yml
 
 eval $(minikube docker-env)
-echo "$CCBLUE >>> build nginx container <<< $CCEND"
+echo "$CCBLUE_BOLD >>> build nginx container <<< $CCEND"
 docker build -t nginx_image srcs/nginx
+
+
+echo "$CCBLUE_BOLD >>> apply kube pods <<< $CCEND"
+minikube addons enable metallb
+kubectl apply -f srcs/nginx.yml
+kubectl apply -f srcs/metallb-configmap.yml
