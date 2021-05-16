cf6a154278c81ea0d3898298e58b40bf5e2a8124
kanlee <kanlee@student.42seoul.kr>
2021-05-17 02:39:21 +0900

    add mysql, connect wordpress with mysql

---
+++ srcs/mysql/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+mysqld_safe & sleep 3
+
+mysql -e "CREATE USER '$WP_DB_USERNAME'@'localhost' identified by '$WP_DB_PASSWORD';" 
+mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;"
+mysql -e "GRANT ALL on $WP_DB_NAME.* to '$WP_DB_USERNAME'@'localhost' identified by '$WP_DB_PASSWORD';"
+mysql -e "GRANT ALL on $WP_DB_NAME.* to '$WP_DB_USERNAME'@'%' identified by '$WP_DB_PASSWORD';"
+
+mysqladmin shutdown
+
+mysqld_safe --user=mysql --datadir=/var/lib/mysql
