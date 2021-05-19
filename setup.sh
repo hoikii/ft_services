db7af757b5f279191f4183011f99ba0cb20161da
kanlee <kanlee@student.42seoul.kr>
2021-05-19 16:28:13 +0900

    ftp without ssl

--- setup.sh
+++ setup.sh
@@ -14,10 +14,11 @@ sed -i '' "s/___MINIKUBE_IP___/$MINIKUBE_IP/" srcs/metallb-configmap.yml
 
 eval $(minikube docker-env)
 echo "$CCBLUE_BOLD >>> build nginx container <<< $CCEND"
-docker build -t nginx_image --build-arg MINIKUBE_IP=`minikube ip` srcs/nginx
+docker build -t nginx_image --build-arg MINIKUBE_IP=$MINIKUBE_IP srcs/nginx
 docker build -t wordpress_image srcs/wordpress
 docker build -t mysql_image srcs/mysql
 docker build -t phpmyadmin_image srcs/phpmyadmin
+docker build -t ftps_image --build-arg MINIKUBE_IP=$MINIKUBE_IP srcs/ftps
 
 
 echo "$CCBLUE_BOLD >>> enable minikube dashboard <<< $CCEND"
@@ -29,6 +30,7 @@ kubectl apply -f srcs/nginx.yml
 kubectl apply -f srcs/wordpress.yml
 kubectl apply -f srcs/mysql.yml
 kubectl apply -f srcs/phpmyadmin.yml
+kubectl apply -f srcs/ftps.yml
 
 echo "run '$CCBLUE minikube dashboard$CCEND ' to open dashboard."
 echo "minikube IP =$CCBLUE `minikube ip` $CCEND"
